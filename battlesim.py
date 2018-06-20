6455f8c3bdaea44fdec05a3853c3a53bed21e026
Andrew Ilersich <ailersic@users.noreply.github.com>
2018-06-20 15:33:58 -0400

    Added demonym to list of polled shards

--- battlesim.py
+++ battlesim.py
@@ -2,12 +2,16 @@ import xml.etree.ElementTree as ET
 import urllib3
 import random
 
+offensiveNationName = "Nandiguo"
+defensiveNationName = "Vittauria"
+strikeLimit = 12
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 user_agent = {'user-agent': 'Python script for simulating roleplay battles, written by the nations of Usimbi and Vittauria'}
 http = urllib3.PoolManager(1, headers = user_agent)
 
 class nation:
-    economyBins = ((33, 66), ("Third world", "Second world", "First world"))
+    economyBins = ((50, 75), ("Third world", "Second world", "First world"))
     armsBins = ((7000, 14000), ("Poor", "Fair", "Industrious"))
     complianceBins = ((50, 75), ("Rebellious", "Moderate", "Slavish"))
     defenceBins = ((2000, 4000), ("Lazy", "Average", "Vigilant"))
@@ -20,16 +24,16 @@ class nation:
         return len(thresholds)
     
     def getAttributes(self):
-        response = http.request('GET', 'www.nationstates.net/cgi-bin/api.cgi?nation=' + self.name + ';q=census;scale=1+13+16+42+46')
+        response = http.request('GET', 'www.nationstates.net/cgi-bin/api.cgi?nation=' + self.name + ';q=demonym+census;scale=1+13+16+42+46')
         XMLstr = response.data
-        
         root = ET.fromstring(XMLstr)
         
-        self.economy =      float(root[0][0][0].text)
-        self.infotech =     float(root[0][1][0].text)
-        self.arms =         float(root[0][2][0].text)
-        self.compliance =   float(root[0][3][0].text)
-        self.defence =      float(root[0][4][0].text)
+        self.demonym =      root[0].text
+        self.economy =      float(root[1][0][0].text)
+        self.infotech =     float(root[1][1][0].text)
+        self.arms =         float(root[1][2][0].text)
+        self.compliance =   float(root[1][3][0].text)
+        self.defence =      float(root[1][4][0].text)
     
     def sortAttributes(self):
         self.economyBin = self.sortIntoBin(self.economy, self.economyBins[0])
@@ -53,75 +57,53 @@ class nation:
     
     def __init__(self, name):
         self.name = name
-        self.demonym = name + "ian"
         print(name + ":")
         
         self.getAttributes()
         self.sortAttributes()
         self.calcStats()
         print("")
-    
-    def __init__(self, name, demonym):
-        self.name = name
-        self.demonym = demonym
-        print(name + ":")
-        
-        self.getAttributes()
-        self.sortAttributes()
-        self.calcStats()
-        print("")
-
-def greet(lang):
-    if lang == "vi":
-        print("Good day to you!")
-    if lang == "ki":
-        print("Ungimua!")
-    if lang == "na":
-        print("Nihao!")
 
 if __name__ == "__main__":
-    greet("vi")
+    offensiveNation = nation(offensiveNationName)
+    defensiveNation = nation(defensiveNationName)
     
-    nation1 = nation("Vittauria", "Vittaurian")
-    nation2 = nation("Nandiguo", "Nandiguese")
-    
-    if nation1.arms > nation2.arms:
-        print(nation1.name + " is ridiculously overpowered compared to " + nation2.name + ".")
-    elif nation1.arms < nation2.arms:
-        print(nation2.name + " is ridiculously overpowered compared to " + nation1.name + ".")
+    if offensiveNation.arms > defensiveNation.arms:
+        print(offensiveNation.name + " is ridiculously overpowered compared to " + defensiveNation.name + ".")
+    elif offensiveNation.arms < defensiveNation.arms:
+        print(defensiveNation.name + " is ridiculously overpowered compared to " + offensiveNation.name + ".")
     else:
-        print(nation1.name + " and " + nation2.name + " are evenly matched in weaponry.")
+        print(offensiveNation.name + " and " + defensiveNation.name + " are evenly matched in weaponry.")
     
-    if nation1.defence > nation2.defence:
-        print(nation2.name + " could not penetrade the defences of " + nation1.name + " with a nuclear bomb.")
-    elif nation1.defence < nation2.defence:
-        print(nation1.name + " could not penetrade the defences of " + nation2.name + " with a nuclear bomb.")
+    if offensiveNation.defence > defensiveNation.defence:
+        print(defensiveNation.name + " could not penetrade the defences of " + offensiveNation.name + " with a nuclear bomb.")
+    elif offensiveNation.defence < defensiveNation.defence:
+        print(offensiveNation.name + " could not penetrade the defences of " + defensiveNation.name + " with a nuclear bomb.")
     else:
         print("Everything depends on what you can throw at one another; the two nations are evenly matched in defence...")
     
-    if nation1.infotech > nation2.infotech:
-        print(nation1.name + " has seen the big board in the " + nation2.demonym + " War Room.")
-    elif nation1.infotech < nation2.infotech:
-        print(nation1.name + " has set up a permanent cocktail bar for " + nation2.demonym + " spies.")
+    if offensiveNation.infotech > defensiveNation.infotech:
+        print(offensiveNation.name + " has seen the big board in the " + defensiveNation.demonym + " War Room.")
+    elif offensiveNation.infotech < defensiveNation.infotech:
+        print(offensiveNation.name + " has set up a permanent cocktail bar for " + defensiveNation.demonym + " spies.")
     else:
         print("The spying game is evenly matched: be careful out there.")
     
     strikeCounter = 1
-    strikeLimit = 12
     print("")
     
-    while nation1.defenceStat > 0 and nation2.defenceStat > 0 and strikeCounter <= strikeLimit:
+    while offensiveNation.defenceStat > 0 and defensiveNation.defenceStat > 0 and strikeCounter <= strikeLimit:
         print("STRIKE " + str(strikeCounter))
         
         if strikeCounter % 2:
-            attacker = nation1
-            defender = nation2
+            attacker = offensiveNation
+            defender = defensiveNation
         else:
-            attacker = nation2
-            defender = nation1
+            attacker = defensiveNation
+            defender = offensiveNation
         
-        attackRoll = random.randint(0,6)
-        defenceRoll = random.randint(0,6)
+        attackRoll = random.randint(0,5)
+        defenceRoll = random.randint(0,5)
         
         if attackRoll < attacker.attackStat:
             print("Successful " + attacker.demonym + " attack!")
@@ -142,14 +124,14 @@ if __name__ == "__main__":
         strikeCounter = strikeCounter + 1
         print("")
     
-    if strikeCounter < strikeLimit:
-        if nation2.defenceStat <= 0:
-            winner = nation1
-            loser = nation2
+    if offensiveNation.defenceStat <= 0 or defensiveNation.defenceStat <= 0:
+        if defensiveNation.defenceStat <= 0:
+            winner = offensiveNation
+            loser = defensiveNation
             print(loser.name + " has lost the battle and the war; " + winner.name + " is victorious!")
-        elif nation1.defenceStat <= 0:
-            winner = nation2
-            loser = nation1
+        elif offensiveNation.defenceStat <= 0:
+            winner = defensiveNation
+            loser = offensiveNation
             print(loser.name + " REALLY should not have started that war, seeing as they have to surrender...")
         
         if winner.economy > loser.economy:
@@ -158,9 +140,9 @@ if __name__ == "__main__":
             print("Reparations and tribute are in order, are they not, " + loser.name + "?")
     else:
         print("A draw! Looks like more diplomacy is needed...")
-        if nation1.economy > nation2.economy:
-            print("Perhaps " + nation1.name + " would be willing to help out?")
-        elif nation1.economy < nation2.economy:
-            print("Perhaps " + nation2.name + " would be willing to help out?")
+        if offensiveNation.economy > defensiveNation.economy:
+            print("Perhaps " + offensiveNation.name + " would be willing to help out?")
+        elif offensiveNation.economy < defensiveNation.economy:
+            print("Perhaps " + defensiveNation.name + " would be willing to help out?")
         else:
             print("Looks like these two idiots will have to clean up their own messes.")
